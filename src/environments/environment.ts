1ac5b85049b088fcc33d752983d40ba7f820bc8e
Jonathan Rivas <jonathan@smartstart.us>
2021-05-17 01:37:37 +0800

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import { environmentInterface } from "environtmentInterface";
 export const environment:environmentInterface = {
   pusher_key:"b36265bb4cc959e12f16",
   cluster: "ap1",
-  // apiUrl: "http://localhost:3000/",
+  // apiUrl: "http://localhost:3000/", 
   apiUrl: "https://explorehub-backend.herokuapp.com/",
   production: false,
   weatherMap_api_key: "0e1b2d0f4c5326bbc4d3f67c769177e9",
